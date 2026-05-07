70585cf3e2e553bccb02962b803a29a9c3acd8d2
4nntdz5x78-droid <4nntdz5x78@privaterelay.appleid.com>
2026-05-07 17:45:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(FRONTEND));
-app.use(express.static(__dirname));
 
 app.use('/leads',    require('./routes/leads'));
 app.use('/admin',    require('./routes/admin'));
@@ -19,9 +18,20 @@ app.use('/books',    require('./books'));
 app.use('/orders',   require('./orders'));
 app.use('/payments', require('./payments'));
 
-app.get('/catalogo.html', (req, res) => res.sendFile(path.join(FRONTEND, 'catalogo.html')));
-app.get('/catalogo',      (req, res) => res.sendFile(path.join(FRONTEND, 'catalogo.html')));
-app.get('/', (req, res) => res.sendFile(path.join(FRONTEND, 'index.html')));
+app.get('/contato-beaver-books/contato-beaver-books.html', (req, res) => 
+  res.sendFile(path.join(FRONTEND, 'contato-beaver-books.html')));
+
+app.get('/catalogo-livro-beaver-books/catalogo.html', (req, res) => 
+  res.sendFile(path.join(FRONTEND, 'catalogo.html')));
+
+app.get('/catalogo.html', (req, res) => 
+  res.sendFile(path.join(FRONTEND, 'catalogo.html')));
+
+app.get('/livro/:id', (req, res) => 
+  res.sendFile(path.join(FRONTEND, 'livro.html')));
+
+app.get('*', (req, res) => 
+  res.sendFile(path.join(FRONTEND, 'index.html')));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
